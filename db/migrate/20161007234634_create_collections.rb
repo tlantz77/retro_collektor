97603d384f16cc7b3292e0d698d37dbc89914e95
Tim Lantz <tlantz@gmail.com>
2016-10-07 19:47:33 -0500

    add model associations and migration fields

--- db/migrate/20161007234634_create_collections.rb
+++ db/migrate/20161007234634_create_collections.rb
@@ -1,6 +1,8 @@
 class CreateCollections < ActiveRecord::Migration
   def change
     create_table :collections do |t|
+      t.string :title
+      t.integer :user_id
 
       t.timestamps null: false
     end
