c0d8f0bd10d0fd3a7fa2e440f884d3ada2dfbb2b
Tim Lantz <tlantz@gmail.com>
2016-10-12 17:02:10 -0500

    basic login/logout working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      @errors = ["Email and/or password is incorrect!"]
+      # render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,11 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user.id] = user.id
+      session[:user_id] = user.id
       redirect_to root_path
     else
       @errors = user.errors.full_messages
-      render :new
+      # render :new
     end
   end
 
