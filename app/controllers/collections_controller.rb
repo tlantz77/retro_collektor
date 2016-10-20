77fa8970a360a867385265437ef6556c6b5acc4d
Tim Lantz <tlantz@gmail.com>
2016-10-20 16:02:39 -0500

    index now user_collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,18 @@
 class CollectionsController < ApplicationController
+
+  def index
+    if current_user
+      render :index
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users do
+    resources :collections
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
 
-  root 'users#index'
+  root "collections#index"
 end
