786173c605a9ba60c218e19fc531ef4f1813f549
Tim Lantz <tlantz@gmail.com>
2016-10-20 17:03:37 -0500

    nested collections routes in users

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,11 +1,16 @@
 class CollectionsController < ApplicationController
 
   def index
+    @user = current_user
     if current_user
       render :index
     else
       redirect_to new_session_path
     end
+  end
+
+  def show
+    @collection = Collection.find(params[:id])
   end
 
   def new
